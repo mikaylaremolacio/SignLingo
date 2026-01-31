c2ccb54aaa41b7672586069f6203166070824c76
NuntichaThummee <nunticha.ppalaloy909@gmail.com>
2026-01-31 18:09:27 -0500

    sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import Login from "./components/Login";
+import Learn from "./components/Learn";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element = {<Login/>}/>
+      <Route path="/learn" element = {<Learn/>}/>
     </Routes>
     </BrowserRouter>
   );
