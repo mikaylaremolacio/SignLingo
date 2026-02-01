5bea9c372542f5f4ec76694f0c5e454fbdc759ab
NuntichaThummee <nunticha.ppalaloy909@gmail.com>
2026-02-01 08:53:36 -0500

    try

--- frontend/src/components/Learn.jsx
+++ frontend/src/components/Learn.jsx
@@ -1,17 +1,24 @@
 import './component.css';
 import signLingoLogo from '../assets/signLingoLogo.png';
 import Navbar from './Navbar';
+<<<<<<< HEAD
 import { useLocation } from 'react-router-dom';
 
 function Learn() {
     const location = useLocation();
     const username = location.state?.username;
     
+=======
+import ASLDetector from "./ASLDetector";
+
+function Learn() {
+
+>>>>>>> main
     return (
         <>
             <Navbar username={username}/>
             <div style={{ backgroundColor: '#0527A6', minHeight: '100vh' }}>
-
+            <ASLDetector />;
             </div>
 
         </>
