3859e7d455ee98297c417a727141d90eb1865b60
ViaYuSecE <via_alexa_yu@yahoo.com>
2026-02-01 04:50:22 -0500

    initial level

--- frontend/src/components/Level.jsx
+++ frontend/src/components/Level.jsx
@@ -22,6 +22,23 @@ function Level() {
         <h1>{username}</h1>
 
       </div>
+
+            <div className="reviewsSection">
+              <div className="reviewsHeader">
+                <span className="reviewsText">1</span>
+              </div>
+              <div className="reviewsCounter">
+                <span className="reviewsNumber">10</span>
+              </div>
+              <button
+                className="sidebarButton startButton"
+                onClick={() => navigate("/reviews", {state: { username }})}
+              >
+                START
+              </button>
+            </div>
+
+
     </>
   );
 }
